321a47cf45cdb58f0607407d55a561c999953a1f
JP <jplmarotinho22@gmail.com>
2025-04-23 19:21:56 -0300

    ProductController completed, all methods

--- src/main/java/jp/progweb/va1/controllers/ProductController.java
+++ src/main/java/jp/progweb/va1/controllers/ProductController.java
@@ -1,15 +1,15 @@
 package jp.progweb.va1.controllers;
 
 import jp.progweb.va1.dtos.ProductCreateDTO;
+import jp.progweb.va1.dtos.ProductUpdateDTO;
 import jp.progweb.va1.models.Product;
 import jp.progweb.va1.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
@@ -17,9 +17,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<List<Product>> listAllProducts() {
+        List<Product> products = productService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable(value = "id") Long id) {
+        Product product = productService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
     @PostMapping
     public ResponseEntity<Product> create(@RequestBody ProductCreateDTO product) {
         Product createdProduct = productService.create(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable(value = "id") Long id,
+            @RequestBody ProductUpdateDTO productUpdateDTO
+    ) {
+        Product updatedProduct = productService.update(id, productUpdateDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product " + id + " deleted successfully");
     }
 }
