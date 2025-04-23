289abe6ec8500ecbfb73e020780df58ff95b3463
JP <jplmarotinho22@gmail.com>
2025-04-23 11:04:50 -0300

    Model Getter and Setter annotation

--- src/main/java/jp/progweb/va1/models/Product.java
+++ src/main/java/jp/progweb/va1/models/Product.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
