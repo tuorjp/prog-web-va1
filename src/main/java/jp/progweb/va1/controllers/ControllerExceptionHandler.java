39e7b3a81696cbeaa9f3e339526fa8d90dfa647d
JP <jplmarotinho22@gmail.com>
2025-04-23 19:20:54 -0300

    ProductAlreadyExistsException on ControllerExceptionHandler

--- src/main/java/jp/progweb/va1/controllers/ControllerExceptionHandler.java
+++ src/main/java/jp/progweb/va1/controllers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package jp.progweb.va1.controllers;
 
 import jp.progweb.va1.services.exceptions.ProductAlreadyExistsException;
+import jp.progweb.va1.services.exceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ProductAlreadyExistsException.class)
     public ResponseEntity<String> handleProductAlreadyExists(ProductAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
