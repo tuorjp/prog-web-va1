a8ca7522f5c1623d95bc340ed46c10e210edcc23
JP <jplmarotinho22@gmail.com>
2025-04-26 13:24:10 -0300

    CrossOrigin front-end

--- src/main/java/jp/progweb/va1/controllers/ProductController.java
+++ src/main/java/jp/progweb/va1/controllers/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
     private final ProductService productService;
 
